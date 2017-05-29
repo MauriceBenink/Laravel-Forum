a839a8128e322708137a20251a79d9386cc0323c
Maurice Benink <mauricebenink@hotmail.com>
2017-05-29 20:32:51 +0200

    0.03.01

--- resources/views/profile/layout/github.blade.php
+++ resources/views/profile/layout/github.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group{{ $errors->has('github') ? ' has-error' : '' }}">
-    <label for="github" class="col-md-4 control-label">GitHub:<b style = font-size:10px;> https://github.com/<i style = color:red>copy paste this</i></b></label>
+    <label for="github" class="col-md-4 control-label">GitHub:<b style = font-size:10px;> https://github.com/<i style = color:red>copy paste this part of the url only</i></b></label>
 
     <div class="col-md-6">
         <input id="github" type="text" class="form-control" name="github" value="{{ isset($object->github)&&!is_null($object->github)?$object->github:old('github')}}" autofocus>
